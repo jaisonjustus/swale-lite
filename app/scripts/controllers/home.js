5884b365909563a2bafdcafbbffc8af25b1334d7
Jaison Justus <jaison.justus.lp@gmail.com>
2013-07-17 00:30:37 +0530

    Added the view page.

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,7 +1,9 @@
 'use strict'
 
 angular.module('Kurippu')
-.controller('HomeController', function($scope, Config, Posts)	{
+.controller('HomeController', function($scope, $rootScope, Config, Posts)	{
 	$scope.config = Config.get();
 	$scope.posts = Posts.get();
+
+	$rootScope.test = "yes";
 });
---
+++ app/scripts/controllers/post.js
@@ -0,0 +1,12 @@
+'use strict'
+
+angular.module('Kurippu')
+.controller('PostController', function($scope, $rootScope, $routeParams, $q, $http)	{
+	
+	console.log($rootScope.test);
+
+	$q.all([$http.get('/posts/' + $routeParams.file + '.md')]).then(function(mdData)	{
+		var selector = angular.element(document.querySelector('.post__content'));
+		selector.append(new Showdown.converter().makeHtml(mdData[0].data));
+	});
+});
