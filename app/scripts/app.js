7a80f73cd7bba9de7de7ae2daf4494c44821f5b6
Jaison Justus <jaison.justus.lp@gmail.com>
2013-07-16 20:17:37 +0530

    AngularJS Project Backbone.

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('kurippuApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
