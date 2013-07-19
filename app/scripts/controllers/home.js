eb820b64eb72bf6d480f534a186b3fc1d5031ecb
Jaison Justus <jaison.justus.lp@gmail.com>
2013-07-19 20:17:04 +0530

    Add gravtar support.
    Add gravtar author image into the blog. Add MD5 library to hash the email to get the gravtar image.

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -3,6 +3,7 @@
 angular.module('Swale')
 .controller('HomeController', function($scope, $rootScope, Config, Posts)	{
 	$scope.config = Config.get();
+	$scope.config.author.email = md5($scope.config.author.email);
 	$scope.posts = Posts.get();
 
 	$rootScope.test = "yes";
