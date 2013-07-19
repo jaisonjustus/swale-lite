fa6710461bbc4f2c02fe1687221db8c57e1b0bd3
Jaison Justus <jaison.justus.lp@gmail.com>
2013-07-19 21:07:37 +0530

    Add social media icons

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -2,9 +2,30 @@
 
 angular.module('Swale')
 .controller('HomeController', function($scope, $rootScope, Config, Posts)	{
+	var socialNetworks = {};
+
 	$scope.config = Config.get();
 	$scope.config.author.email = md5($scope.config.author.email);
 	$scope.posts = Posts.get();
+	$scope.socials = {};
+	
+	var urls = {
+		dribble : "http://dribble.com/",
+		facebook : "http://facebook.com/",
+		flickr : "http://flickr.com/",
+		github : "http://github.com/",
+		gplus : "http://gplus.com/",
+		twitter : "http://twitter.com/",
+		linkedin : "http://linkedin.com/",
+		youtube : "http://youtube.com/"
+	};
+
+	socialNetworks = $scope.config.social;
+	for(var network in socialNetworks)	{
+		if(socialNetworks[network] !== "")	{
+			$scope.socials[network] = urls[network] + socialNetworks[network];
+		}
+	}
 
 	$rootScope.test = "yes";
 });
